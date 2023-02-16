251cb41f5b3adcd85e8db6803cf49e5ee235527b
gipsy <ter4444@gmail.com>
2023-02-16 19:44:42 +0200

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+export default {
+  root: path.resolve(__dirname, 'src'),
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+      '~components': path.resolve(__dirname, 'src/scss/components')
+    }
+  },
+  server: {
+    port: 8080,
+    hot: true
+  }
+}
